9f702dc4fd8c3ccea731d423309d2d0484eed6f7
Colin Balfour <75389927+ColinBalfour@users.noreply.github.com>
2023-12-21 15:16:33 -0500

    removed speedylooncycle :( very sad day

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2024DriveBase";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 35;
+  public static final String GIT_SHA = "16fb5684d7b7c27b8a6aefb5a604d7e12610801c";
+  public static final String GIT_DATE = "2023-12-21 12:12:23 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2023-12-21 13:57:35 EST";
+  public static final long BUILD_UNIX_TIME = 1703185055167L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
