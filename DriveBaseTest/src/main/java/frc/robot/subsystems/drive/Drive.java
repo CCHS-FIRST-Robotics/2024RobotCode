64dcefdd81d81533b26da485b80120a19c9ec9b7
ssaak18 <sammy.saak@gmail.com>
2024-01-31 15:11:41 -0500

    Update Drive.java

--- DriveBaseTest/src/main/java/frc/robot/subsystems/drive/Drive.java
+++ DriveBaseTest/src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -9,34 +9,35 @@ import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public class Drive extends SubsystemBase {
 
     private Module[] modules;
     private ChassisSpeeds speeds;
     private SwerveModuleState state;
+    private Rotation2d angle;
+    private double speedMetersPerSecond;
     
 
     public Drive() {
         speeds = new ChassisSpeeds();
         modules = new Module[4];
+        angle = new Rotation2d(2);
+        speedMetersPerSecond = 0;
 
         for (int i = 0; i < modules.length; i++) {
-            
+            modules[i] = new Module(i);
         }
+
+        state = new SwerveModuleState(speedMetersPerSecond, angle);
     }
 
     public void periodic(){
-        /*
-         * need 4 modules
-         * drive for each one
-         * 
-         * 
-         * what is rotation2d
-         * 
-         * class for modules you wrote
-         */
-
-        
+    
+        for (Module module : modules) {
+            module.drive(state);
+        }
 
     }
     
