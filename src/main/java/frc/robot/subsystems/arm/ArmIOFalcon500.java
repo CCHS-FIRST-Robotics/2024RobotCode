194af7fcd1f75f966c17de137154592b3cf1056f
Billy826 <dgu26@concordcarlisle.org>
2024-02-15 15:01:05 -0500

    Push!
    Bowling!

--- src/main/java/frc/robot/subsystems/arm/ArmIOFalcon500.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOFalcon500.java
@@ -1,6 +1,11 @@
 package frc.robot.subsystems.arm;
 
-import org.littletonrobotics.junction.AutoLog;
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Celsius;
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Volts;
 
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.Orchestra;
@@ -10,19 +15,19 @@ import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.fasterxml.jackson.databind.JsonSerializable.Base;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.units.*;
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Voltage;
 
-import static edu.wpi.first.units.Units.*;
 
-import java.util.function.Supplier;
+
 
 public class ArmIOFalcon500 implements ArmIO {
     /* MOTOR CONTROLLERS + PID */
@@ -35,6 +40,12 @@ public class ArmIOFalcon500 implements ArmIO {
     private static final double driveFeedforwardKs = 0;
     private static final double driveFeedforwardKv = 0;
     private static final double driveFeedforwardKa = 0;
+
+    // I rise! FeedFwd momment!
+    private static final int measuredKWhenHorizontal = 0; // TODO: Measure + fill
+    private static final double kTicksPerRadian = 0; // TODO: Measure + fill
+    private static final double maxGravityFF = 0;
+
 
 
     private final MotionMagicVoltage driveMotionMagic = new MotionMagicVoltage(0);
@@ -75,6 +86,8 @@ public class ArmIOFalcon500 implements ArmIO {
         driveMMConfig.MotionMagicAcceleration = 10; // 1 second to reach max speed
         driveMMConfig.MotionMagicJerk = 30; // .1 seconds to reach max accel
 
+
+        
         // Feedforward momment!
         driveFeedforward = new ArmFeedforward(
             driveFeedforwardKs,
@@ -133,17 +146,25 @@ public class ArmIOFalcon500 implements ArmIO {
         // for testing, dont let the arm go past 90 degrees in either direction
         // positionRad = Radians.of(MathUtil.clamp(positionRad.in(Radians), -Math.PI/2.0, Math.PI/2.0));
         positionRad = Radians.of(MathUtil.clamp(positionRad.in(Radians), Math.PI/6, Math.PI/3));  //uhh probably incorrect fix?
-        driveFalcon.setControl(driveMotionMagic.withPosition(positionRad.in(Rotations)).withSlot(0));
+        // driveFalcon.setControl(driveMotionMagic.withPosition(positionRad.in(Rotations)).withSlot(0));
 
         // lol this is NOT going to work
-        driveFalcon.setControl(driveMotionMagic.withFeedForward(
-            driveFeedforward.calculate(
-                positionRad.in(Radians),
-                0)
-            ).withPosition(
-                positionRad.in(Rotations)).
-            withSlot(0)
-        );
+
+        // driveFalcon.setControl(driveMotionMagic.withFeedForward(
+        //     driveFeedforward.calculate(
+        //         positionRad.in(Radians),
+        //         0)
+        //     ).withPosition(
+        //         positionRad.in(Rotations)).
+        //     withSlot(0)
+        // );
+
+        double cosineScalar = Math.cos(
+            (Radians.convertFrom(driveFalcon.getPosition().getValue(), Rotations) - measuredKWhenHorizontal)
+            / kTicksPerRadian);
+
+        driveFalcon.setControl(driveMotionMagic.withPosition(positionRad.in(Rotations))
+                            .withFeedForward(maxGravityFF * cosineScalar).withSlot(0));
         
 
         
