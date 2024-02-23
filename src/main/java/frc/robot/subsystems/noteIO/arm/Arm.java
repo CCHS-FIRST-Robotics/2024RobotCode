2bfbdf6416897bfd9830fb69c66210b8459a2649
ssaak18 <sammy.saak@gmail.com>
2024-02-23 12:13:53 -0500

    align to shoot at a pos

--- src/main/java/frc/robot/subsystems/noteIO/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/noteIO/arm/Arm.java
@@ -4,17 +4,24 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Twist2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.Constants;
 import frc.robot.Constants.AutoPathConstants;
+import frc.robot.utils.DriveTrajectory;
 
 import static edu.wpi.first.units.Units.*;
 
+import java.util.ArrayList;
 import java.util.function.Supplier;
 
 import org.littletonrobotics.junction.Logger;
 
+import com.choreo.lib.Choreo;
+import com.choreo.lib.ChoreoTrajectory;
+import com.choreo.lib.ChoreoTrajectoryState;
 import com.ctre.phoenix6.SignalLogger;
 
 import edu.wpi.first.units.*;
@@ -79,11 +86,11 @@ public class Arm extends SubsystemBase {
         return new Translation2d(armLength, new Rotation2d(getArmAngle().in(Radians))).plus(armOffset);
     }
 
-    // used to set stuff for suto events
-    public void setCurrentArmPos(Supplier<Translation2d> translationToTargetGround, Supplier<Pose3d> targetPose) {
-        this.translationToTargetGround = translationToTargetGround;
-        this.targetPose = targetPose;
-    }
+    // // used to set stuff for suto events
+    // public void setCurrentArmPos(Supplier<Translation2d> translationToTargetGround, Supplier<Pose3d> targetPose) {
+    //     this.translationToTargetGround = translationToTargetGround;
+    //     this.targetPose = targetPose;
+    // }
 
     public Command alignWithTarget(Supplier<Translation2d> translationToTargetGround, Supplier<Pose3d> targetPose) {
         return run(() -> {
@@ -94,19 +101,35 @@ public class Arm extends SubsystemBase {
         });
     }
 
-    // for auto events
-    public Command autoAlignWithTarget() {
-        return run(() -> {
-            Translation2d armOffset = getArmOffset();
-            Translation2d tranlationToTargetHigh = new Translation2d(this.translationToTargetGround.get().getNorm(), this.targetPose.get().getZ());
-            Rotation2d targetArmAngle = tranlationToTargetHigh.minus(armOffset).getAngle();
-            setArmAngle(Radians.of(Math.PI/2.0 - targetArmAngle.getRadians())); // add 90 degrees since 0 is vertical
-        });
+    public Command getPosFromPath(String path, double eventTime) {
+        ChoreoTrajectory choreoTrajectory = Choreo.getTrajectory(path);
+
+        double timeToEnd = choreoTrajectory.getTotalTime();
+
+        for (int i = 0; i < (int) (timeToEnd / Constants.PERIOD) + 2; i++) {
+            double time = i * Constants.PERIOD;
+            ChoreoTrajectoryState state = choreoTrajectory.sample(time);
+
+            if (time >= eventTime) {
+                // so this is not the ideal way to do it with time but i feel like you could do something simialr w/out time
+                // especially if the em are not using time anymore (which still needs to be switched whoops)
+                Translation2d translationToTargetGround = new Translation2d(state.x, state.y);
+                Pose3d targetPose = new Pose3d(new Pose2d(state.x, state.y, new Rotation2d(state.heading)));
+                return run(() -> {
+                    Translation2d armOffset = getArmOffset();
+                    Translation2d tranlationToTargetHigh = new Translation2d(translationToTargetGround.getNorm(), targetPose.getZ());
+                    Rotation2d targetArmAngle = tranlationToTargetHigh.minus(armOffset).getAngle();
+                    setArmAngle(Radians.of(Math.PI/2.0 - targetArmAngle.getRadians())); // add 90 degrees since 0 is vertical
+                });
+            }
+        }
+
+        return null;
     }
 
-    public Command alignForHandoff() {
+    public Command getMoveAngleCommand(Measure<Angle> angle) {
         return run(() -> {
-            setArmAngle(Radians.of(AutoPathConstants.ARM_HANDOFF_ANGLE));
+            setArmAngle(angle);
         });
     }
 
