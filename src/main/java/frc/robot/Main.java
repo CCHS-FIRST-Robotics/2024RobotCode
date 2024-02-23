b8de8d8089f2fb4b47830266adb4b86f58e06071
Colin Balfour <75389927+ColinBalfour@users.noreply.github.com>
2024-02-23 06:01:50 -0500

    just your average comments

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -11,20 +11,21 @@ import edu.wpi.first.wpilibj.RobotBase;
  * 
  * arm stuff
  * for example actually writing a controlwithJoysticks command
+ * ^ thats not really necessary tbh, check my texts in the gc with david
  * 
  * shooter:
  * Proper feedforward and feedback using the Phoenix API for velocity control,
- * using exponential motion magic (check the Phoenix docs for details)
+ * using exponential motion magic (check the Phoenix docs for details) <--- not doing this anymore, just regular VelocityVoltage control
  * A sysid routine and signal logger export for motor data (should be pretty
  * much identical to how the arm was done, David you might want to do this one)
  * 
- * ! ask colin if intake and shoot commands should move the arm to angle
- * ! ask colin about conflicting button mappings in robotcontainer
- * ! ask colin why shooter even needs motionmagic
+ * ! ask colin if intake and shoot commands should move the arm to angle (yes)
+ * ! ask colin about conflicting button mappings in robotcontainer (fix them)
+ * ! ask colin why shooter even needs motionmagic (it doesn't)
  * 
- * ! ask colin if the neos for arm intake will have encoders
- * ! shooter motion magic stuff
- * ! intake shoot() command
+ * ! ask colin if the neos for arm intake will have encoders (NEOs are brushless motors, so yes)
+ * ! shooter motion magic stuff (see above)
+ * ! intake shoot() command (see comments)
  */
 public final class Main {
     private Main() {
