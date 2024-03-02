cffc7836b266b45d45839f68bf99940d4ad89cf3
Alexandre Hus-Shao <alexjun2008@gmail.com>
2024-03-02 17:41:35 -0500

    velocity control does NOT work yet

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -150,7 +150,7 @@ public class RobotContainer {
         // shoot
         controller.b().onTrue(
                 // prime shooter
-                new InstantCommand(() -> shooter.start(Volts.of(4)), shooter)
+                new InstantCommand(() -> shooter.start(RotationsPerSecond.of(10)), shooter)
                         // wait until shooter is up to speed
                         .alongWith(Commands.waitUntil(shooter::upToSpeed))
                         // shoot
